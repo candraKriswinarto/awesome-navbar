d6d6ff3849989961d98548024d8762a28d99fcf5
candra kriswinarto <ckriswinarto@gmail.com>
2019-11-29 21:29:58 -0500

    Finish the project

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const navbar = document.getElementById('navbar');
+const mobileNavbar = document.querySelector('.mobile-nav');
 const links = document.querySelectorAll('.nav-links li a');
+const burgerIcon = document.querySelector('.burger-icons');
 
 window.onscroll = () => {
   if(document.documentElement.scrollTop > 80) {
@@ -8,7 +10,8 @@ window.onscroll = () => {
     navbar.style.boxShadow = "0 1px 2px rgba(0,0,0,.5)";
     links.forEach(link => {
       link.style.color = "#111";
-    });
+		});
+		burgerIcon.style.top = '1rem';
 
   } else {
     navbar.style.backgroundColor = "transparent";
@@ -16,6 +19,11 @@ window.onscroll = () => {
     navbar.style.boxShadow = "none";
     links.forEach(link => {
       link.style.color = "#fff";
-    });
+		});
+		burgerIcon.style.top = '1.3rem';		
   }
 }
+
+burgerIcon.addEventListener('click', () => {
+	mobileNavbar.classList.toggle('show-nav');
+});
